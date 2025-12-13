b54b63005a359a0245288909bbdf3392c4b71650
uxtuahgp <alex_dubrovsky@mail.ru>
2025-12-14 00:37:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def index(request: Request, ip_address: Optional[str] = Depends(get_client_ip)):
     try:
         with get_db_connection() as db:
             cursor = db.cursor()
-            query = "INSERT INTO "{db_table}" (request_date, request_ip) VALUES (%s, %s)"
+            query = "INSERT INTO "+db_table+" (request_date, request_ip) VALUES (%s, %s)"
             values = (current_time, final_ip)
             cursor.execute(query, values)
             db.commit()
@@ -121,7 +121,7 @@ def get_requests():
     try:
         with get_db_connection() as db:
             cursor = db.cursor()
-            query = "SELECT id, request_date, request_ip FROM "{db_table}" ORDER BY id DESC LIMIT 50"
+            query = "SELECT id, request_date, request_ip FROM "+db_table+" ORDER BY id DESC LIMIT 50"
             cursor.execute(query)
             records = cursor.fetchall()
             cursor.close()
